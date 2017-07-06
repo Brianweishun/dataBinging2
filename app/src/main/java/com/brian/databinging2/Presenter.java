c5967c07a6a035a85a5a1603277b8b7cecc29a7d
brian_chen <brian_chen@gemteks.com>
2017-07-07 00:46:50 +0800

    Event Handling-Listener Bindings: add view for parameter

--- app/src/main/java/com/brian/databinging2/Presenter.java
+++ app/src/main/java/com/brian/databinging2/Presenter.java
@@ -1,13 +1,14 @@
 package com.brian.databinging2;
 
 import android.util.Log;
+import android.view.View;
 
 /**
  * Created by Brian_Chen on 06/07/2017.
  */
 public class Presenter {
-  public void onSaveClick(User user){
+  public void onSaveClick(View view, User user){
     //Listener Bindings
-    Log.d("brian", "presenter onSaveClick" + user.getFirstName());
+    Log.d("brian", "presenter onSaveClick " + user.getFirstName() + view.);
   }
 }
